71311b57598fb33fcf59b75a41769e19893ea6d1
Bill Nguyen <ivecleandedmyroom@gmail.com>
2023-05-02 18:48:19 +1000

    Added requirements for our project

--- SDD Assignment Task 2.py
+++ SDD Assignment Task 2.py
@@ -3,4 +3,10 @@ from tkinter import *
 #this is the start of the project
 """
 Topic: Doing a software that helps keep score for cricket games
+
+Your program must keep track of the two opposing teams. The user should be able to enter data on a ball-by-ball basis. 
+Each ball should result in either a hit (with an amount of runs attached), a wide, a no-ball or a pass ball (with an amount of runs attached). 
+The program should keep track of overs, providing indication when it’s time to swap ends. It should also keep track of outs, 
+storing the bowler’s name whenever a batter gets out.
+
 """
